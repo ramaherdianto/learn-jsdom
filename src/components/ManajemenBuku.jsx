c281726524682c3f084f87bc49ff2b76cc264483
ramaherdianto <ramadhanherdianto35@gmail.com>
2022-03-22 14:12:57 +0700

    membuat component ManajemenBuku

---
+++ src/components/ManajemenBuku.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function ManajemenBuku() {
+  // PART DATA
+  // PART EVENT HANDLING
+
+  return (
+    <div className="container mt-3">
+      <h1 className="text-center">Manajemen Buku</h1>
+
+      <button className="btn btn-sm btn-primary my-2">Tambah Buku</button>
+
+      {/* {Input From} */}
+      <div id="form"></div>
+
+      {/* {tabel data buku} */}
+    </div>
+  );
+}
+
+export default ManajemenBuku;
