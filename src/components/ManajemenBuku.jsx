8d575e764e9159e9df6006ff77d534deb3212682
ramaherdianto <ramadhanherdianto35@gmail.com>
2022-03-30 19:22:09 +0700

    membuat form inputan menjadi hide dan muncul ketika tombol tambah di klik

--- src/components/ManajemenBuku.jsx
+++ src/components/ManajemenBuku.jsx
@@ -1,34 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import TabelBuku from "./TabelBuku";
 
 function ManajemenBuku() {
   // PART DATA
+  const [formMode, setFormMode] = useState("");
 
   // PART EVENT HANDLING
+  function showCreateForm() {
+    setFormMode("show");
+  }
 
   return (
     <div className="container mt-3">
       <h1 className="text-center">Manajemen Buku</h1>
 
-      <button className="btn btn-sm btn-primary my-2">Tambah Buku</button>
+      <button className="btn btn-sm btn-primary my-2" onClick={showCreateForm}>
+        Tambah Buku
+      </button>
 
       {/* {Input From} */}
-      <div id="form" className="card py-2 my-3 bg-secondary">
-        <div className="card-body">
-          <h4>Form Buku</h4>
-          <form action="" className="row">
-            <div className="col-6">
-              <input type="text" name="judul" className="form-control mx-2" placeholder="Judul..." />
-            </div>
-            <div className="col-4">
-              <input type="text" name="pengarang" placeholder="Pengarang..." className="form-control" />
-            </div>
-            <div className="col-2">
-              <input type="submit" value="Submit" className="btn btn-success" />
-            </div>
-          </form>
+      {formMode === "show" && (
+        <div id="form" className="card py-2 my-3 bg-secondary">
+          <div className="card-body">
+            <h4>Form Buku</h4>
+            <form action="" className="row">
+              <div className="col-6">
+                <input type="text" name="judul" className="form-control mx-2" placeholder="Judul..." />
+              </div>
+              <div className="col-4">
+                <input type="text" name="pengarang" placeholder="Pengarang..." className="form-control" />
+              </div>
+              <div className="col-2">
+                <input type="submit" value="Submit" className="btn btn-success" />
+              </div>
+            </form>
+          </div>
         </div>
-      </div>
+      )}
 
       {/* {tabel data buku} */}
       <TabelBuku></TabelBuku>
